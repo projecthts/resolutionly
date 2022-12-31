e37231005fe6203a690b380731f2dfc225e5b54f
anika-001 <dream.sweeter.30.09@gmail.com>
2022-12-31 18:13:40 +0530

    sign updated

--- webapp/resolutionly/src/app/signin/signin.component.ts
+++ webapp/resolutionly/src/app/signin/signin.component.ts
@@ -1,132 +1,63 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
-import {  FormControl,  FormGroup,  Validators,  AbstractControl,} from '@angular/forms';
+import { FormControl, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
-import { logindata, regdata } from '../JSONdata/signin';
+import { logindata, regdata } from '../JsonData/signin';
 import { AuthService } from '../services/auth.service';
-
-
 
 @Component({
   selector: 'app-signin',
   templateUrl: './signin.component.html',
-  styleUrls: ['./signin.component.scss'],
+  styleUrls: ['./signin.component.scss']
 })
 export class SigninComponent implements OnInit {
- 
-  constructor(private as: AuthService, private router: Router) {}
 
+  constructor(private as: AuthService, private router: Router) { }
+
+  
   login: boolean = true;
-  error: string = '';
-  errormessage: any;
+  error: any;
+  errormessage:any;
+
   selectedrole: any;
   signindata: any;
   signupdata: any;
- 
 
   move() {
     this.login = !this.login;
   }
 
   submit() {
-    if (this.login) {
-      if (!this.formlogin.invalid) {
-        this.as
-          .login(this.formlogin.value)
-          .then((res) => {
-            this.as.getUserState().subscribe(res => {
-              if(res){
-                this.as.getprofile(res.uid).subscribe((res:any) => {
-                  if(res.payload.data().role == 1){this.router.navigate(['/home']);}
-                  else if(res.payload.data().role == 2) this.router.navigate(['/schedule']);
-                  else {
-                     this.router.navigate(['/form'],{ queryParams: { id: 7}})
-                  }
-                })
-              }
-            })
-          })
-          .catch((err) => {
-            console.log(err);
-          });
-      } else {
-        console.log('invalid login');
-        this.error = 'Please fill all fields correctly.';
-      }
-    } else {
-      if (!this.formreg.invalid) {
-        let data = this.formreg.value;
-          data['role'] = this.signupdata[5].value;
-          this.as.signup(data)
-          .then((res) => {
-            this.login = true;
-          })
-          .catch((err) => {
-            this.error = err.message;
-          });
-      }
-      else{
-        console.log('invalid signup');
-        this.error = 'Please fill all fields correctly.';
-      }
+    if(this.login){
+      this.as.login(this.formlogin.value).then(res => {
+        this.router.navigate(['/home']);
+      })
     }
-    
+    else{
+      this.formreg.get("role")?.setValue(this.signupdata[5].value);
+      this.as.signup(this.formreg.value).then(res => {
+        this.router.navigate(['/home']);
+      })
+    }
   }
 
-  
   formlogin = new FormGroup({
-    email: new FormControl('', [
-      Validators.required,
-      Validators.pattern(
-        '^(([^<>()[\\]\\\\.,;:\\s@\\"]+(\\.[^<>()[\\]\\\\.,;:\\s@\\"]+)*)|(\\".+\\"))@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$'
-      ),
-    ]),
-    password: new FormControl('', [Validators.required]),
-  });
+    email: new FormControl(''),
+    password: new FormControl('')
+  })
 
-  formreg = new FormGroup(
-    {
-      name: new FormControl('', [Validators.required]),
-      email: new FormControl('', [
-        Validators.required,
-        Validators.pattern(
-          '^(([^<>()[\\]\\\\.,;:\\s@\\"]+(\\.[^<>()[\\]\\\\.,;:\\s@\\"]+)*)|(\\".+\\"))@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$'
-        ),
-      ]),
-      phone: new FormControl('', [
-        Validators.required,
-        Validators.pattern('[2-9]{2}\\d{8}'),
-      ]),
-      password: new FormControl('', [
-        Validators.required,
-        Validators.pattern(
-          /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,15}$/
-        ),
-      ]),
-      confirmpassword: new FormControl('', [Validators.required]),
-    },
-    { validators: this.checkPasswords }
-  );
-
+  formreg = new FormGroup({
+    name: new FormControl(''),
+    email: new FormControl(''),
+    phone: new FormControl(''),
+    password: new FormControl(''),
+    role: new FormControl('user'),
+    confirmpassword: new FormControl(''),
+  })
+  
   ngOnInit(): void {
     this.signindata = logindata;
-    this.signupdata = regdata;    
-  }
-  
-
-  formlog(name: string) {
-    return this.formlogin.get(name)!;
-  }
-  formregget(name: string) {
-    return this.formreg.get(name)!;
+    this.signupdata = regdata;
   }
 
-  checkPasswords(group: AbstractControl) {
-    // here we have the 'passwords' group
-    let pass = group.get('password')?.value;
-    let confirmPass = group.get('confirmpassword')?.value;
-
-    console.log(pass, confirmPass, pass == confirmPass);
-    return pass === confirmPass ? null : { notSame: true };
-  }
 }
