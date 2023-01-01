6934b501d0d4bd6b30c2cdfd44d8d7442ee9ecae
Vaishnavi <vaishnavisdesai@gmail.com>
2023-01-01 16:44:26 +0530

    letter to future

--- webapp/resolutionly/src/app/app-routing.module.ts
+++ webapp/resolutionly/src/app/app-routing.module.ts
@@ -3,12 +3,17 @@ import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { GmapsComponent } from './gmaps/gmaps.component';
 import { HomeComponent } from './home/home.component';
+import { LetterToFutureSelfComponent } from './letter-to-future-self/letter-to-future-self.component';
 import { SigninComponent } from './signin/signin.component';
 
 const routes: Routes = [
   {
     path: 'signin',
     component: SigninComponent
+  },
+  {
+    path: 'resolutions',
+    component: LetterToFutureSelfComponent
   },
   {
     path: 'journal',
--- webapp/resolutionly/src/app/letter-to-future-self/letter-to-future-self.component.ts
+++ webapp/resolutionly/src/app/letter-to-future-self/letter-to-future-self.component.ts
@@ -14,6 +14,7 @@ export class LetterToFutureSelfComponent implements OnInit {
   constructor(private as: AuthService, private db: AngularFirestore, private router: Router) { }
 
   user: any;
+  resolutions: any = [];
 
   ngOnInit(): void {
     this.as.getUserState().subscribe(res => {
@@ -21,6 +22,7 @@ export class LetterToFutureSelfComponent implements OnInit {
       this.as.getprofile(res?.uid).then((res:any) => {
         this.user = res.payload.id;
         console.log(this.user);
+        this.getresolutions();
         // this.getMoods();
         // this.getstories();
       })
@@ -32,8 +34,38 @@ export class LetterToFutureSelfComponent implements OnInit {
     reminder: new FormControl(' ')
   })
 
+  resolutionform = new FormGroup({
+    resolution: new FormControl(' '),
+    progress: new FormControl(' ')
+  })
+
+  getresolutions(){
+    this.db.collection("Users").doc(this.user).collection("Resolutions").snapshotChanges().subscribe(res => {
+      this.resolutions = res;
+    })
+  }
   addstory(){
     var date = new Date()
-    this.db.collection("Users").doc(this.user).collection("Stories").doc(date.getFullYear().toString()).set({ "letterToSelf": "" })
+    this.db.collection("Users").doc(this.user).collection("LetterToSelf").doc(date.getFullYear().toString()).set({ "letterToSelf": this.letterform.get("content")?.value, "date":  this.letterform.get("reminder")?.value}).then(res => {
+      this.letterform.get("content")?.setValue("");
+      this.letterform.get("reminder")?.setValue("");
+    })
+  }
+
+  addResolution(){
+    this.db.collection("Users").doc(this.user).collection("Resolutions").add({ "resolution": this.resolutionform.get("resolution")?.value, "progress":  this.resolutionform.get("progress")?.value}).then(res => {
+      this.resolutionform.get("resolution")?.setValue("");
+      this.resolutionform.get("progress")?.setValue("");
+    })
+  }
+
+  editResolution(x:any){
+    this.resolutionform.get("resolution")?.setValue(x.payload.doc.data().resolution);
+    this.resolutionform.get("progress")?.setValue(x.payload.doc.data().progress);
+    this.deleteResolution(x.payload.doc.id);
+  }
+
+  deleteResolution(id:any){
+    this.db.collection("Users").doc(this.user).collection("Resolutions").doc(id).delete();
   }
 }
