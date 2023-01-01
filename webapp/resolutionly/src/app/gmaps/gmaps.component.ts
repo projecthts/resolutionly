828ee4b4528213a3d2efe008c5d6a0bb1250c3d7
anika-001 <dream.sweeter.30.09@gmail.com>
2023-01-01 13:41:17 +0530

    good Ui thereee

---
+++ webapp/resolutionly/src/app/gmaps/gmaps.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-gmaps',
+  templateUrl: './gmaps.component.html',
+  styleUrls: ['./gmaps.component.scss']
+})
+export class GmapsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
