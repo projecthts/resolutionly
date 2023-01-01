a4217828043789392f5616d3d6b762e5a1d41257
Vaishnavi <vaishnavisdesai@gmail.com>
2023-01-01 14:38:22 +0530

    firebase utility

--- backend/src/ViewModels/FirebaseUtilities.ts
+++ backend/src/ViewModels/FirebaseUtilities.ts
@@ -4,8 +4,50 @@ import { keys } from '../support/Keys'
 
 class Firebase {
 
-    public app;
+    public app = initializeApp.initializeApp(keys.firebaseConfig);
+    public db = firestore.getFirestore(this.app);
+    public collection = firestore.collection;
+    public onSnapshot = firestore.onSnapshot;
+    public query = firestore.query;
+    public updateDoc = firestore.updateDoc;
+    public doc = firestore.doc;
+    public setDoc = firestore.setDoc;
+    public addDoc = firestore.addDoc;
+
     constructor(){
-        this.app = initializeApp.initializeApp(keys.firebaseConfig);
+        
+    }
+
+    public addData() : Promise<any>{
+        return new Promise((resolve, reject) => {
+            this.setDoc(this.doc(this.db, "cities", "LA"), {
+                name: "Los Angeles",
+                state: "CA",
+                country: "USA"
+              })
+              .then((res: any) => {
+                resolve(res)
+              })
+              .catch((err) => {
+                reject(err)
+              })
+        })
+    }
+
+    public addResolution(phoneNumber: string, resolution: string) : Promise<any>{
+        return new Promise((resolve, reject) => {
+            this.addDoc(this.collection(this.db, `Users/${phoneNumber}/Resolutions/`), {
+                resolution: resolution,
+                progress: 0
+              })
+              .then((res: any) => {
+                resolve(res)
+              })
+              .catch((err) => {
+                reject(err)
+              })
+        })
     }
 }
+
+export let firebaseUtilities = new Firebase();
